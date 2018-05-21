999988b6bde959d8b2c416d4d15ed811f736c3fb
Heloá Meneses <h.demeneses@zoover.nl>
2018-05-21 12:01:28 +0200

    Create, save and edit store functionalities

--- initial-state/controllers/storeController.js
+++ initial-state/controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage  = (req, res) => {
   res.render('index');
 };
@@ -6,6 +9,28 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store'});
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  await store.save();
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+}
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  // TODO: confirm they're the owner of the store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+}
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, // return the new store instead of the old one
+    runValidators: true
+   }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
 };
